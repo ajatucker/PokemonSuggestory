fbd35fcfd2895dffa2d227b3fbb1e4c8c00a5752
alexistucker <ajatucker@outlook.com>
2022-04-15 22:21:14 -0400

    Update SamplePokemonSelect.sql

--- CIS421-PokemonDB/SQL/SamplePokemonSelect.sql
+++ CIS421-PokemonDB/SQL/SamplePokemonSelect.sql
@@ -1,3 +1,3 @@
 ﻿SELECT [pName],[pDesc],[pType1],[pType2], [evolutionName]
 FROM [Pokemon],[PokemonType], [Evolutions]
-WHERE [Pokemon].[id] = 1 AND [PokemonType].[id] = 1 AND [Evolutions].[id] = 1;
+WHERE [Pokemon].[id] = [PokemonType].[id] AND [Pokemon].[id] = [Evolutions].[id];
